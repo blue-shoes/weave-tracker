41058f4c689300bf93fa2df972a4c7ea4e1963f3
Adam Scharf <adam.scharf@studsvik.com>
2022-09-25 13:07:56 -0400

    Setting up program variables in main

---
+++ main.py
@@ -0,0 +1,19 @@
+# main.py -- put your code here!
+
+from project import Project
+from pimoroni import Button
+from picographics import PicoGraphics, DISPLAY_PICO_DISPLAY, PEN_RGB332
+
+display = PicoGraphics(display=DISPLAY_PICO_DISPLAY, pen_type=PEN_RGB332, rotate=90)
+backlight = 0.5
+display.set_backlight=backlight
+display.set_font("sans")
+
+button_a = Button(12)
+button_b = Button(13)
+button_x = Button(14)
+button_y = Button(15)
+
+WIDTH, HEIGHT = display.get_bound()
+
+
