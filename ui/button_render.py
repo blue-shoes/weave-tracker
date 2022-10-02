02ed68cca24d6c26e1c1d2bc330f1b43b45538f0
Adam Scharf <adam.scharf@studsvik.com>
2022-10-02 15:41:40 -0400

    Big updates to button render. No enums (not supported in micropython), realign text, reverse vertical sense to align with reality

--- ui/button_render.py
+++ ui/button_render.py
@@ -1,26 +1,22 @@
-from ..main import display, WIDTH, HEIGHT
-from enum import Enum
+TOP_LEFT = 1,
+TOP_RIGHT = 2,
+BOTTOM_LEFT = 3,
+BOTTOM_RIGHT = 4
 
-class ButtonEnum(Enum):
-    TOP_LEFT = 1,
-    TOP_RIGHT = 2,
-    BOTTOM_LEFT = 3,
-    BOTTOM_RIGHT = 4
-
-def place_button(text, width, height, button_placement, width_pad=2, height_pad=2):
-    if button_placement == ButtonEnum.TOP_LEFT:
-        display.text(text, width/2 + width_pad, HEIGHT - (height/2 + height_pad))
-        display.line(0, HEIGHT-(height + 2*height_pad), (width + 2*width_pad), HEIGHT-(height + 2*height_pad))
-        display.line(width + 2*width_pad, HEIGHT, width + 2(width_pad), HEIGHT-(height + 2*height_pad))
-    elif button_placement == ButtonEnum.TOP_RIGHT:
-        display.text(text, WIDTH - (width/2 + width_pad), HEIGHT - (height/2 + height_pad))
-        display.line(WIDTH, HEIGHT-(height + 2*height_pad), WIDTH - (width + 2*width_pad), HEIGHT-(height + 2*height_pad))
-        display.line(WIDTH - (width + 2*width_pad), HEIGHT, WIDTH - (width + 2(width_pad)), HEIGHT-(height + 2*height_pad))
-    elif button_placement == ButtonEnum.BOTTOM_LEFT:
-        display.text(text, width/2 + width_pad, (height/2 + height_pad))
-        display.line(0, (height + 2*height_pad), (width + 2*width_pad), (height + 2*height_pad))
-        display.line(width + 2*width_pad, 0, width + 2(width_pad), (height + 2*height_pad))
-    elif button_placement == ButtonEnum.BOTTOM_RIGHT:
-        display.text(text, WIDTH - (width/2 + width_pad), (height/2 + height_pad))
-        display.line(WIDTH, (height + 2*height_pad), WIDTH - (width + 2*width_pad), (height + 2*height_pad))
-        display.line(WIDTH - (width + 2*width_pad), 0, WIDTH - (width + 2(width_pad)), (height + 2*height_pad))
+def place_button(hardware, text, width, height, button_placement, width_pad=2, height_pad=2):
+    if button_placement == TOP_LEFT:
+        hardware.display.text(text, int(width/2) + width_pad, hardware.HEIGHT - int(height/2 + height_pad))
+        hardware.display.line(0, hardware.HEIGHT-(height + 2*height_pad), (width + 2*width_pad), hardware.HEIGHT-(height + 2*height_pad))
+        hardware.display.line(width + 2*width_pad, hardware.HEIGHT, width + 2(width_pad), hardware.HEIGHT-(height + 2*height_pad))
+    elif button_placement == TOP_RIGHT:
+        hardware.display.text(text, hardware.WIDTH - int(width/2 + width_pad), hardware.HEIGHT - int(height/2 + height_pad))
+        hardware.display.line(hardware.WIDTH, hardware.HEIGHT-(height + 2*height_pad), hardware.WIDTH - (width + 2*width_pad), hardware.HEIGHT-(height + 2*height_pad))
+        hardware.display.line(hardware.WIDTH - (width + 2*width_pad), hardware.HEIGHT, hardware.WIDTH - (width + 2(width_pad)), hardware.HEIGHT-(height + 2*height_pad))
+    elif button_placement == BOTTOM_LEFT:
+        hardware.display.text(text, int(width/2) + width_pad, int(height/2 + height_pad))
+        hardware.display.line(0, (height + 2*height_pad), (width + 2*width_pad), (height + 2*height_pad))
+        hardware.display.line(width + 2*width_pad, 0, width + 2(width_pad), (height + 2*height_pad))
+    elif button_placement == BOTTOM_RIGHT:
+        hardware.display.text(text, hardware.WIDTH - int(width/2 + width_pad), int(height/2 + height_pad))
+        hardware.display.line(hardware.WIDTH, (height + 2*height_pad), hardware.WIDTH - (width + 2*width_pad), (height + 2*height_pad))
+        hardware.display.line(hardware.WIDTH - (width + 2*width_pad), 0, hardware.WIDTH - (width + 2(width_pad)), (height + 2*height_pad))
