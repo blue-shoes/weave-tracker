500b2be46314d89a1fcfdbec0cde82ad0f6bf01e
Adam Scharf <adam.scharf@studsvik.com>
2022-10-11 09:55:46 -0400

    Fixes for drawing graphics lines horizontally

--- ui/setup.py
+++ ui/setup.py
@@ -165,15 +165,15 @@ class Stage2():
     
     def set_up(self, lever_num):
         self.hardware.display.circle(self.margin + self.lever_width*lever_num + self.radius + 3, self.hardware.LEVER_DIST, self.radius)
-        self.hardware.display.line(self.margin + self.lever_width*lever_num + 3, self.hardware.HEIGHT - self.hardware.LEVER_DIST, self.margin + self.lever_width*lever_num + 3 + 2*self.radius, self.hardware.LEVER_DIST)
+        self.hardware.display.line(self.margin + self.lever_width*lever_num + 3, self.hardware.HEIGHT - self.hardware.LEVER_DIST, self.margin + self.lever_width*lever_num + 3 + 2*self.radius, self.hardware.HEIGHT - self.hardware.LEVER_DIST)
     
     def set_down(self, lever_num):
         self.hardware.display.circle(self.margin + self.lever_width*lever_num + self.radius + 3, self.hardware.HEIGHT - self.hardware.LEVER_DIST, self.radius)
-        self.hardware.display.line(self.margin + self.lever_width*lever_num + 3, self.hardware.LEVER_DIST, self.margin + self.lever_width*lever_num + 3 + 2*self.radius, self.hardware.HEIGHT - self.hardware.LEVER_DIST)
+        self.hardware.display.line(self.margin + self.lever_width*lever_num + 3, self.hardware.LEVER_DIST, self.margin + self.lever_width*lever_num + 3 + 2*self.radius, self.hardware.LEVER_DIST)
 
     def clear_lever(self, lever_num):
         self.hardware.set_bg_pen()
-        self.hardware.display.rectangle(self.margin + self.lever_width*lever_num, self.hardware.LEVER_DIST - self.radius, 2*(self.radius+3), self.hardware.HEIGHT - 2*(self.hardware.LEVER_DIST))
+        self.hardware.display.rectangle(self.margin + self.lever_width*lever_num, self.hardware.LEVER_DIST - self.radius, 2*(self.radius+3),  self.hardware.HEIGHT - 2*(self.hardware.LEVER_DIST))
         self.hardware.set_fg_pen()
 
 def setup(hardware, project):
